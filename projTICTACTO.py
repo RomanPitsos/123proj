903e4d890af3782550b6e825f954b8b5b3754116
RomanPitsos <romanos.pitsos@gmail.com>
2023-07-06 14:31:40 +0300

    Update projTICTACTO.py

--- projTICTACTO.py
+++ projTICTACTO.py
@@ -31,14 +31,14 @@ def player_input(board):
 # check win/tie
 
 def checkRow(board):
-    global winner
+    
     if (board[0]==board[1]==board[2] and board[0]!="-") or (board[3]==board[4]==board[5] 
         and board[3]!="-") or (board[6]==board[7]==board[8] and board[6]!="-"):
         
         return True   
     
 def checkLine(board):
-    global winner
+    
     if (board[0]==board[3]==board[6] and board[0]!="-") or (board[1]==board[4]==board[7] 
         and board[1]!="-") or (board[2]==board[5]==board[8] and board[2]!="-"):
         
